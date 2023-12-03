151cc510aac224c2cf30458e70db1003fbe196c0
alex <alex.kaes@gmail.com>
2023-12-02 22:04:03 -0800

    updated git actions and minor changes to main.tf

--- terraform/environment/development/main.tf
+++ terraform/environment/development/main.tf
@@ -3,10 +3,10 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = "-rg"
+  name     = "project-one-rg"
   location = "WEST US 2"
 
   tags = {
-    environment = "Dev"
+    environment = "development"
   }
 }
