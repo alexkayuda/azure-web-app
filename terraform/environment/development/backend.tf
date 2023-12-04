52d75dfa2206821f49c3af2839f3c31a3b6d53d1
alex <alex.kaes@gmail.com>
2023-12-03 16:43:41 -0800

    github action backend v10

--- terraform/environment/development/backend.tf
+++ terraform/environment/development/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "tfstates"
     storage_account_name = "aktfstateaccount"
     container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    key                  = "terraform.tfstate"
   }
 }
