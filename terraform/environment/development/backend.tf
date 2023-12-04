2dc4de337cca86a653f569e2b2888d4b7f7d2c2e
alex <alex.kaes@gmail.com>
2023-12-03 16:25:21 -0800

    github action backend v8

--- terraform/environment/development/backend.tf
+++ terraform/environment/development/backend.tf
@@ -1,10 +1,4 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.8"
-    }
-  }
   backend "azurerm" {
     use_msi = true
     resource_group_name  = "tfstates"
