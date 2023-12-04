268be06890fc00cb8923d2464cc33d586619b2f9
alex <alex.kaes@gmail.com>
2023-12-03 16:24:00 -0800

    github action backend v7

--- terraform/environment/development/backend.tf
+++ terraform/environment/development/backend.tf
@@ -6,6 +6,7 @@ terraform {
     }
   }
   backend "azurerm" {
+    use_msi = true
     resource_group_name  = "tfstates"
     storage_account_name = "aktfstateaccount"
     container_name       = "tfstate"
