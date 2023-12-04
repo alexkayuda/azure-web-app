00b9fe48ec51a1d6465966a6353fff0a2db58b92
alex <alex.kaes@gmail.com>
2023-12-03 16:14:06 -0800

    github action backend v6

--- terraform/environment/development/backend.tf
+++ terraform/environment/development/backend.tf
@@ -1,7 +1,14 @@
-
-backend "azurerm" {
-  resource_group_name  = "tfstates"
-  storage_account_name = "aktfstateaccount"
-  container_name       = "tfstate"
-  key                  = "dev.terraform.tfstate"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.8"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tfstates"
+    storage_account_name = "aktfstateaccount"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
