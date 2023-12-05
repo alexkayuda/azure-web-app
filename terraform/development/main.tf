56bd42546ea051c42807a0a5ed1823efb03486ee
alex <alex.kaes@gmail.com>
2023-12-04 21:38:22 -0800

    terraform module added vnet v4

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -4,3 +4,12 @@ module "resource_group" {
   environment         = var.environment
   resource_group_name = var.resource_group_name
 }
+
+module "virtual_network" {
+  source                        = "../modules/vnet"
+  depends_on                    = [module.resource_group]
+  virtual_network_name          = var.virtual_network_name
+  virtual_network_address_space = var.virtual_network_address_space
+  virtual_network_location      = var.location
+  resource_group_name           = module.resource_group.resource_group_name
+}
